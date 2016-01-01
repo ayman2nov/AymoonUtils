faaee1a219a579f43c15a396fc515079dce38fad
Ayman Mohammed <aymanMohamed@eg.ibm.com>
2016-01-01 17:34:27 -0500

    added isProbablyArabic method

--- src/com/aymoon/util/AymoonUtils.java
+++ src/com/aymoon/util/AymoonUtils.java
@@ -78,4 +78,15 @@ public class AymoonUtils {
         }
         return refined;
     }
+
+    public static boolean isProbablyArabic(String s) {
+        for (int i = 0; i < s.length();) {
+            int c = s.codePointAt(i);
+            if (c >= 0x0600 && c <= 0x06E0) {
+                return true;
+            }
+            i += Character.charCount(c);
+        }
+        return false;
+    }
 }
