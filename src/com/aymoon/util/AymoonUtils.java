019c5e1a72a2d7a3e13c2d41ffea4d1679995121
Ayman Mohammed <ayman2nov@gmail.com>
2018-03-31 22:27:47 -0400

    fixed package

--- src/com/aymoon/util/AymoonUtils.java
+++ src/com/aymoon/util/AymoonUtils.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.homains.utils.hackforme;
+package com.aymoon.util;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
